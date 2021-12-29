4c379e8b2a3be1855d805cbacec1e9d5e1a0c1a1
Rampratap <rampratapsingh_kushwah@intuit.com>
2021-12-29 09:08:25 +0530

    Changes has been done

---
+++ src/com/interview/CountPairsWithDiff.java
@@ -0,0 +1,26 @@
+package com.interview;
+
+public class CountPairsWithDiff {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int arr[] = {1, 5, 3, 4, 2};
+        int n = arr.length;
+        int k = 3;
+        System.out.println("Count of pairs with given diff is "
+                + countPairsWithDiffK(arr, n, k));
+    }
+
+    private static int countPairsWithDiffK(int[] arr, int n, int k) {
+
+        int count = 0;
+        for(int i= 0;i<arr.length;i++){
+            for(int j = i+1;j<n;j++) {
+                if (arr[i] - arr[j] == k || arr[j]-arr[i] == k){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
