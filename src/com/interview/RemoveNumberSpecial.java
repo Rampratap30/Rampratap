28c8a910ce6e95f5151626cd98de28e5ae43a012
Rampratap <rampratapsingh_kushwah@intuit.com>
2022-01-08 21:40:23 +0530

    Updated done

--- src/com/interview/RemoveNumberSpecial.java
+++ src/com/interview/RemoveNumberSpecial.java
@@ -3,8 +3,8 @@ package com.interview;
 public class RemoveNumberSpecial {
     public static void main(String[] args)
     {
-        String str = "3157g0862fA";
-        String afterReplace  = str.replaceAll("([a-zA-Z13579])", "");
-        System.out.println(afterReplace);
+        String str = "3157g08#62%fA";
+        String afterReplace  = str.replaceAll("[^A-Za-z0-9]", "");
+        System.out.println(afterReplace.replaceAll("[(a-zA-Z13579)]",""));
     }
 }
